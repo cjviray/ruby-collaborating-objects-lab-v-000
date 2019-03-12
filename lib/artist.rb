8494759670188dcd68d0d4e4b81e68abdc9de704
Christian V <viray.christianj@gmail.com>
2019-03-12 20:58:34 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,53 +1,28 @@
 class Artist
   attr_accessor :name, :songs
-  
-  @@all = [] 
-  
+  @@all = []
   
   def initialize(name)
-    @name = name
+    @name = name 
     @songs = []
   end 
   
+  def self.all
+    @@all 
+  end 
+  
+  def self.find_or_create_by_name(artist_name)
+    artist = @@all.find {|a| a.name == artist_name}  # the way that find works, says go through array #all and return all the objects that makes this statement true
+  end 
   
   def add_song(song)
     @songs << song
+    song.artist = self 
   end 
-  
   
   def save
     @@all << self
   end 
   
-  
-  def self.all
-    @@all
-  end 
-  
-  
-  def self.find_or_create_by_name(name)
-    if self.find_by_name(name)
-      self.find_by_name(name)
-    else
-      self.create_by_name(name)
-    end 
-  end 
-  
-  
-  def self.find_by_name(name)
-    self.all.find { |artist| artist.name == name }
-  end
-
-
-  def self.create_by_name(name)
-    artist = Artist.new(name)
-    artist.save
-    artist
-  end 
-  
-  
-  def print_songs
-    @songs.each { |song| puts song.name }
-  end 
   
 end 
